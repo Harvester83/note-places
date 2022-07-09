f5fb83835da5da25022e2b5dd48400aecf9ca805
Rashad Suleymanov <rashadsuleymanov@risk.az>
2022-07-09 23:38:00 +0400

    Note css.modules redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import React from "react";
+import ol from 'ol';
+import styles from './Counter.module.css'
+import { Counter } from "./components/Counter";
+// import Map from 'ol/map';
+// import layer from 'ol/layer';
+// import proj from 'ol/proj';
+// import extent from 'ol/extent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="contained">Contained</Button>
+// import ol from "openlayers";
+// import { layer, Map, Layers } from "react-openlayers";
 
-        <Typography variant="h5" gutterBottom component="div">
-          h5. Heading
-        </Typography>
-      </header>
+var resolutions = [];
+var matrixIds = [];
+// var proj3857 = proj.get("EPSG:3857");
+// var maxResolution = extent.getWidth(proj3857!.getExtent()) / 256;
+
+// for (var i = 0; i < 18; i++) {
+//   matrixIds[i] = i.toString();
+//   resolutions[i] = maxResolution / Math.pow(2, i);
+// }
+
+// var tileGrid = new ol.tilegrid.WMTS({
+//   origin: [-20037508, 20037508],
+//   resolutions: resolutions,
+//   matrixIds: matrixIds
+// });
+
+// // Key restricted to localhost and wz6wl262l7.codesandbox.io
+// var key = "z3s6k7fy9u7m1u1daeewymau";
+
+// var ign_source = new ol.source.OSM({
+//   url: "https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png"
+// });
+
+// const App = () => (
+//   <Map view={{ center: [0, 0], zoom: 2 }}>
+//     <Layers>
+//       <layer.Tile />
+//     </Layers>
+//   </Map>
+// );
+
+const App = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col">1</div>
+      <div className="col">
+        <p className={styles.error}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea nemo similique
+          delectus est debitis consectetur distinctio labore sint ipsa minus, obcaecati, repudiandae ipsum nisi rem magni. Accusamus doloremque nesciunt totam
+        </p>
+      </div>
+      
+      <Counter />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
+
